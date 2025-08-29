67d498112040a46411f4aadbedd00f95fbfed54a
manojdm018 <manojdm018@gmail.com>
2025-08-29 19:43:07 +0530

    maths header practice

---
+++ mathheader.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int x = 90;
+    //x = sqrt(x); // here sqrt() function allows you to get square root of certain number
+
+    //x = pow(x, 2); //here the pow() function allows you to get power function like 2 to the power of 2
+
+    //x = round(x); // round function as name suggests round ups the decimal number
+
+    //x = ceil(x);  // here the ceil function roundup the number upwards like for x =3.14 o/p would be 4
+
+    //x = floor(x);  //here the floor function round towards the less for example above x =3.14 o/p would be 3
+
+    //x = abs(x); // here the abs() means absoulte which has the ability to convert negitive number to postive.
+
+    // x = log(x); // here the log() function givees the logorithum value of specific value 
+
+    x = sin(x);
+
+    printf("%d",x);
+}
