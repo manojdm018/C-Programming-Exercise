df8f025db4d20e46601ef95ae6fb0dcf75504199
manojdm018 <manojdm018@gmail.com>
2025-08-30 19:58:02 +0530

    this is a if else with bool as an example

---
+++ ifelsebool.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool manoj = true;
+
+
+    if (manoj)
+    {
+        printf("He is a student at GCU");
+    }
+    else{
+        printf("He is NOT a student at GCU");
+    }
+    return 0;
+}
