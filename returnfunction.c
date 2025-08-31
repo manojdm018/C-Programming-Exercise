73240d62a1a3562c18d55714295a4eb49eb734e5
manojdm018 <manojdm018@gmail.com>
2025-08-31 23:38:21 +0530

    Return function examples

---
+++ returnfunction.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+// we'll learn about return function in this program
+
+/*int addition(int x,int y){   // this is example 1
+    return x + y;
+}*/
+
+/*bool ageVerify(int num){
+    if (num >= 18){
+        return true;     //this is example 2
+    }
+    else{
+        return false;
+    }
+}*/
+
+int main()
+{
+   /* int x ;
+    int y ;
+    int result = addition(4,5);   // this is example 1
+    printf("%d",result);*/
+   
+   /* int age = 17;
+    if (ageVerify(age))
+    {
+        printf("You are adult");    // this is example 2
+    }
+    else{
+        printf("Growup kid");
+    }*/
+    
+    return 0;
+}
