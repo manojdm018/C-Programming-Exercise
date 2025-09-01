9dfa5f7123692b7be5d734b6adb03d63b1905090
manojdm018 <manojdm018@gmail.com>
2025-09-01 23:09:39 +0530

    This is the first excersise of for loop

---
+++ forloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    for (int i = 10;i>=1;i--){
+        printf("%d\n", i);
+        Sleep (1000);
+    }
+    printf("HAPPY BIRTHDAY!!");
+    return 0 ;
+}
